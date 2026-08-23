c094330e46a968a2173c69d507939266e87ca624
Bhuwan Upadhyay <bhuwan.upadhyay.work@gmail.com>
2026-08-23 16:54:30 +0530

    chore: added tailwindcss and daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import AppRoutes from "@/routes/AppRoutes"
 
 const App = () => {
-  return   <AppRoutes/>
+  return  (
+    <div data-theme="sunset">
+      <AppRoutes/>
+    </div>
+  )
 }
 
 export default App
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import toast from 'react-hot-toast'
 
 const HomePage = () => {
+
+  const btnTempHandler = ()=>{
+    toast.success("congrats");
+  }
+
   return (
-    <div>
-      HomePage
+    <div >
+      <button onClick={btnTempHandler} className="btn">Button</button>
+      <button onClick={btnTempHandler} className="btn btn-neutral">Neutral</button>
+      <button onClick={btnTempHandler} className="btn btn-primary">Primary</button>
+      <button onClick={btnTempHandler} className="btn btn-secondary">Secondary</button>
+      <button onClick={btnTempHandler} className="btn btn-accent">Accent</button>
+      <button onClick={btnTempHandler} className="btn btn-ghost">Ghost</button>
+      <button onClick={btnTempHandler} className="btn btn-link">Link</button>
     </div>
   )
 }
